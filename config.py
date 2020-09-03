7f8743a0b28d4064ec488581bd1dec8cba7bd590
johnwhelchel <johnwhelchel@users.noreply.github.com>
2020-09-03 19:58:16 -0400

    Seems to make a pdf

---
+++ config.py
@@ -0,0 +1,4 @@
+import pathlib
+
+ARTICLES_ROOT = pathlib.Path(
+    "/Users/johnwhelchel/Dropbox/laptop/Documents/21-30 Education/22 Library/22.19 The Economist")
---
+++ create_pdf.py
@@ -0,0 +1,3 @@
+from pdf.creator import create_latest
+
+create_latest()
