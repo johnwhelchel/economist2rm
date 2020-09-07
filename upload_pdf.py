f514605fdbbc85598aacea89db8dbd558439bd5d
johnwhelchel <johnwhelchel@users.noreply.github.com>
2020-09-07 09:22:41 -0400

    Upload works

---
+++ upload_pdf.py
@@ -0,0 +1,7 @@
+from remarkable.uploader import Uploader
+
+if __name__ == '__main__':
+    uploader = Uploader()
+    uploader.upload_file_to_folder(
+        "/Users/johnwhelchel/Documents/21-30 Education/22 Library/22.19 The Economist/What Putin fears | Aug 29th 2020/What Putin fears | Aug 29th 2020.pdf",
+        "Economist")
